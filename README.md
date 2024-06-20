0a57c66967bcaf52ca54df3be629ea7fed43c203
putrinatasyaa <pnatasya77@gmail.com>
2024-06-20 07:54:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# qgis2web_2024_06_20-07_48_04_665770
+ Perkebunan Kelapa Sawit Sumsel
---
+++ data/PERKEBUNANOGANKOMERINGULU_14.js
@@ -0,0 +1,1 @@
+var json_PERKEBUNANOGANKOMERINGULU_14 = {"type":"FeatureCollection","name":"PERKEBUNANOGANKOMERINGULU_14","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
